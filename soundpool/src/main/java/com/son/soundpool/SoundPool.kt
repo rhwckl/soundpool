5321128c14cefe4d0a261c4cee21ae68a59ed2ac
son <rhwckl@gmail.com>
2017-02-21 12:25:59 +0900

    :sparkles: add SoundPool.kt

--- soundpool/src/main/java/com/son/soundpool/SoundPool.kt
+++ soundpool/src/main/java/com/son/soundpool/SoundPool.kt
@@ -1,7 +1,37 @@
 package com.son.soundpool
 
+import android.content.Context
+import android.media.AudioAttributes
+
 /**
  * Created by songc on 2017/02/21.
+ * @param maxStreams 同時に再生できる効果音の数。デフォルトでは５つ。
  */
-class SoundPool {
+class SoundPool(maxStreams: Int = 5) {
+    val audioAttributes = AudioAttributes
+            .Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+    val soundPool = android.media.SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(maxStreams)
+            .build()
+
+    val aliases = mutableMapOf<String, Int>()
+
+    fun load(context: Context, resourceId: Int, stringId: String) {
+        aliases[stringId] = soundPool.load(context, resourceId, 1)
+    }
+
+    fun unload(stringId: String) {
+        aliases[stringId]?.let { soundPool.unload(it) }
+    }
+
+    fun play(stringId: String) {
+        aliases[stringId]?.let {
+            soundPool.play(it, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+    }
 }
